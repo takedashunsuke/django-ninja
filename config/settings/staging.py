311abdd9f396072f189847129a0048755af6402d
bien-takeda-at-207198706198 <shunsuke.takeda@exhiwork.co.jp>
2024-12-02 20:44:55 +0900

    config_rere

--- config/settings/staging.py
+++ config/settings/staging.py
@@ -8,6 +8,12 @@ INSTALLED_APPS += [
     'debug_toolbar',
 ]
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 ACCOUNT_EMAIL_VERIFICATION = 'none'  # メール認証なし
 
 FRONTEND_URL = "https://mysite-eesq.onrender.com"
