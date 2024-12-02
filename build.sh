cb661a0d1fab73f3a96f5539dcbf0e556956cf23
bien-takeda-at-207198706198 <shunsuke.takeda@exhiwork.co.jp>
2024-12-02 20:55:19 +0900

    config_re6

--- build.sh
+++ build.sh
@@ -7,9 +7,9 @@ pip install --upgrade pip
 pip install -r requirements.txt
 
 # Collect static files (optional, only if needed for production)
-python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput --settings=config.settings.staging
 
 # Apply database migrations
-python manage.py migrate
+python manage.py migrate --settings=config.settings.staging
 
 # python manage.py createsuperuser
--- config/settings/staging.py
+++ config/settings/staging.py
@@ -8,9 +8,14 @@ INSTALLED_APPS += [
     'debug_toolbar',
 ]
 
+# SQLite の設定
 DATABASES = {
-    "default": config("DATABASE_URL", default=default_dburl, cast=dburl),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
+
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
