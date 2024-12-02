a8fbd4653d187302e1deafc99db836aa8ac229e8
bien-takeda-at-207198706198 <shunsuke.takeda@exhiwork.co.jp>
2024-12-02 20:24:50 +0900

    config

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # Set the Django settings module to staging
-export DJANGO_SETTINGS_MODULE=config.settings.staging
+# export DJANGO_SETTINGS_MODULE=config.settings.staging
 
 # Install dependencies
 pip install --upgrade pip
 pip install -r requirements.txt
 
 # Collect static files (optional, only if needed for production)
-python manage.py collectstatic --noinput
+python manage.py collectstatic --settings config.settings.staging --noinput
 
 # Apply database migrations
-python manage.py migrate
+python manage.py migrate --settings config.settings.staging
 
 # python manage.py createsuperuser
--- config/settings/staging.py
+++ config/settings/staging.py
@@ -1,4 +1,3 @@
-
 ACCOUNT_EMAIL_VERIFICATION = 'none'  # メール認証なし
 
 FRONTEND_URL = "https://mysite-eesq.onrender.com"
