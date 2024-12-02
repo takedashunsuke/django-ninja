448bde4ec6b988d422713efc64e75703a33bd261
bien-takeda-at-207198706198 <shunsuke.takeda@exhiwork.co.jp>
2024-12-02 20:18:07 +0900

    render.yaml_v9

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Set the Django settings module to staging
+export DJANGO_SETTINGS_MODULE=config.settings.staging
 
 # Install dependencies
 pip install --upgrade pip
@@ -10,4 +12,6 @@ python manage.py collectstatic --noinput
 # Apply database migrations
 python manage.py migrate
 
-python manage.py createsuperuser
+# python manage.py createsuperuser
+# Create superuser with a stronger password
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('test@gmail.com', 'test', 'Str0ngP@ssw0rd!2024')" | python manage.py shell
---
+++ config/settings/staging.py
@@ -0,0 +1,4 @@
+
+ACCOUNT_EMAIL_VERIFICATION = 'none'  # メール認証なし
+
+FRONTEND_URL = "https://mysite-eesq.onrender.com"
