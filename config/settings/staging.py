d4c1aeb7a4b450ef67c4dfcc30740fe9610aa480
bien-takeda-at-207198706198 <shunsuke.takeda@exhiwork.co.jp>
2024-12-02 23:10:04 +0900

    staging.py

--- config/settings/staging.py
+++ config/settings/staging.py
@@ -3,19 +3,16 @@ from .base import *
 DEBUG = True
 ALLOWED_HOSTS = ['*']
 
-INSTALLED_APPS += [
-    'debug_toolbar',
-]
 
 # SQLite 設定を引き継ぐ
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+#     }
+# }
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
