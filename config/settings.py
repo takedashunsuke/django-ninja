c574ac359d3aaca3ff2106557241c8c8b9c63899
bien-takeda-at-207198706198 <shunsuke.takeda@exhiwork.co.jp>
2024-12-03 08:11:18 +0900

    settings.pyに戻す4static

--- config/settings.py
+++ config/settings.py
@@ -154,7 +154,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 # STATIC_ROOT = str(BASE_DIR / "staticfiles")
 STATIC_ROOT = '/var/www/{}/static'.format(PROJECT_NAME)
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = "/media/"
 
